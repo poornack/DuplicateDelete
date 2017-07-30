1fd0a90e998b46ddda3dfb692e72d3ed4eb5d41e
Poorna Chander Kalidas <Poorna@Poornas-MacBook-Pro.local>
2017-07-29 19:33:49 -0700

    First commit. Script will find and print in the console duplicate .jpg and .mts files.

---
+++ DuplicateDelete.py
@@ -0,0 +1,112 @@
+import hashlib
+import os
+import glob
+import string
+
+#*************
+#  Begin Def
+#*************
+
+#Print a Dict where its elements are Strings. Options param indent to set the number of spaces to put in front of a line      
+def printDict(inputDict, indent = 0):
+    
+    space = ""
+    
+    for x in range(indent):
+        space += " "
+    
+    for keys,values in inputDict.items():
+        print space + keys + "      " + values
+
+#Print the duplicate Dict findDuplicates() returns.   
+def printDuplicateDict(inputDict):
+
+    for keys,values in inputDict.items():
+        #print keys + ":   "
+        print "{}:".format(keys)
+        for filename in values:
+            print "    {}".format(filename)
+            
+        print ""
+        
+#Finds duplicates and enters it in a Dict and returns it     
+def findDuplicates(inputDict):
+    
+    dictOfDuplicates = {}
+    
+    numberOfDuplicates = 0
+    duplicateFound = 0
+    
+    
+    for keyOriginal,valueOriginal in inputDict.items():
+        
+        listOfDuplicates = []
+        
+        for keyComparer,valueComparer in inputDict.items():
+            if valueOriginal == valueComparer and keyOriginal != keyComparer:
+                
+                listOfDuplicates.append(keyComparer)
+                del inputDict[keyComparer]
+                duplicateFound = 1
+        
+        if duplicateFound == 1:
+            numberOfDuplicates += 1;
+            listOfDuplicates.append(keyOriginal)
+            dictOfDuplicates[numberOfDuplicates] = listOfDuplicates
+            
+            del inputDict[keyOriginal]
+            duplicateFound = 0
+            
+            
+            
+    return dictOfDuplicates
+#*************
+#  End Def
+#*************
+
+#****************
+#  Begin Script
+#****************
+
+#Has to be entered the as the parameters for glob. Not used at the moment
+fileTypesToCheck = ["*.jpg", "*.MTS"] 
+
+directoryToCheck = raw_input("Enter the directory to check: ")
+
+#Change working directory to inputted value
+os.chdir(directoryToCheck)
+
+#Add file extention to check. This is case sensitive
+listOfJpgAndMts = []
+listOfJpgAndMts.extend(glob.glob("*.JPG"))
+listOfJpgAndMts.extend(glob.glob("*.jpg"))
+listOfJpgAndMts.extend(glob.glob("*.MTS"))
+listOfJpgAndMts.extend(glob.glob("*.mts"))
+
+#Initialize dict to hold file name and crc
+crc = {}
+
+#Calculate crc for each file in  list and store in dict
+for file in listOfJpgAndMts:
+    m = hashlib.md5()
+    m.update(open(file, 'rb').read())
+    crc[file] = m.hexdigest()
+
+print "List of JPGs and MTS files in directory:"
+printDict(crc, 6)
+print "\n"
+
+#Find duplicates
+dictOfDuplicates = findDuplicates(crc)
+print "List of duplicated found:"
+printDuplicateDict(dictOfDuplicates)
+
+
+#Deleting duplicate files is yet to be implemented
+
+
+    
+#****************
+#  End Script
+#****************
+    
---
+++ README.md
@@ -0,0 +1,14 @@
+#Duplicate Delete
+
+This application can be used to find duplicate files in a folder. Currently, the file extention it finds are hard coded to find .jpg and .mts files. Support for all other file extentions is progress
+
+To do:
+- Add support for all file formats
+- Add the option to delete duplicate files
+- Add logging
+- Add progress. Helpful for long operations.
+
+How it works:
+The CRC of each file in the directory is read and compared with the other files' CRC. The file name does not have to be similar.
+
+Note: This has only been tested with MacOS. There may be issues when running on Window. Feel free to send me bugs you find and any other feedback
